5b9a3ea986a44e3899d4c6f3a12979edeafe775f
BSydo <sidorenko.bogdan@gmail.com>
2020-07-10 16:44:37 +0200

    +flask-login base form

--- config.py
+++ config.py
@@ -1,11 +1,4 @@
-# input image dimensions
-img_rows = 28
-img_cols = 28
-input_shape = (1, img_rows, img_cols)
-X_astype = 'float32'
+import os
 
-# number of classes
-num_classes = 10
-
-batch_size = 128
-epochs = 12
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
